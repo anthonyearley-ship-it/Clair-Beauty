7dff2046555162c956bcd8b6b88f24b07dcfa445
anthonyearley-ship-it <anthony.earley@gmail.com>
2026-03-24 20:15:32 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
           'Authorization': `Bearer ${process.env.RESEND_API_KEY}`
         },
         body: JSON.stringify({
-          from: 'Clair Beauty Intelligence <hello@clairbeautyco.com>',
+          from: 'Clair <clair@clairbeautyco.com>',
           to: email,
           subject: subject,
           html: html
